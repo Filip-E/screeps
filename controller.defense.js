f57c21d277592b65b48085ba39eb13083cdf7c47
FilipElen <filipelengit@outlook.com>
2020-03-22 20:40:19 +0100

    defense bugfix1

--- controller.defense.js
+++ controller.defense.js
@@ -5,7 +5,7 @@ module.exports = {
         var hostiles = room.find(FIND_HOSTILE_CREEPS);
         if (hostiles.length > 0) {
             var username = hostiles[0].owner.username;
-            Game.notify(`User ${username} spotted in room ${roomName}`);
+            Game.notify(`User ${username} spotted in room ${room}`);
             var towers = room.find(
                 FIND_MY_STRUCTURES, { filter: { structureType: STRUCTURE_TOWER } });
             towers.forEach(tower => tower.attack(hostiles[0]));
