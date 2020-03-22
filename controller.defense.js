9b16933c67b4d4e5331a36e3ac02377b8876dd95
FilipElen <filipelengit@outlook.com>
2020-03-22 20:42:39 +0100

    defense bugfix2

--- controller.defense.js
+++ controller.defense.js
@@ -5,12 +5,13 @@ module.exports = {
         var hostiles = room.find(FIND_HOSTILE_CREEPS);
         if (hostiles.length > 0) {
             var username = hostiles[0].owner.username;
-            Game.notify(`User ${username} spotted in room ${room}`);
+            //Game.notify(`User ${username} spotted in room ${room}`);
+            console.log(`User ${username} spotted in room ${room}`);
             var towers = room.find(
                 FIND_MY_STRUCTURES, { filter: { structureType: STRUCTURE_TOWER } });
             towers.forEach(tower => tower.attack(hostiles[0]));
             
-            let defenders = _(Game.creeps).filter({ memory: { role: 'harvester' }}).value();
+            let defenders = _(Game.creeps).filter({ memory: { role: 'defender' }}).value();
             for(let key in defenders){
                 if (defenders[key].attack(hostiles[0]) == ERR_NOT_IN_RANGE) {
                     defenders[key].moveTo(hostiles[0], { visualizePathStyle: { stroke: '#ffaa00' } });
